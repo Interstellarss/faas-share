296743c695791e5006f679ac66b98af980cc1cde
Puxuan <leslie.qq.lol@gmail.com>
2022-05-24 16:36:41 +0200

    changing namespace to faas-share

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -23,11 +23,11 @@ if [ $PASSWORD_EXISTS -eq 0 ]; then
     PASSWORD=$(head -c 16 /dev/urandom| $sha_cmd | cut -d " " -f 1)
     echo -n $PASSWORD > password.txt
 
-    kubectl --context "kind-$DEVENV" -n openfaas create secret generic basic-auth \
+    kubectl --context "kind-$DEVENV" -n faas-share create secret generic basic-auth \
     --from-literal=basic-auth-user=admin \
     --from-literal=basic-auth-password="$PASSWORD"
 else
-    PASSWORD=$(kubectl get secret -n openfaas basic-auth -o=go-template='{{index .data "basic-auth-password"}}' | base64 --decode)
+    PASSWORD=$(kubectl get secret -n faas-share basic-auth -o=go-template='{{index .data "basic-auth-password"}}' | base64 --decode)
     echo -n $PASSWORD > password.txt
 fi
 
@@ -42,9 +42,9 @@ echo "Waiting for helm install to complete."
 helm upgrade \
     --kube-context "kind-$DEVENV" \
     --install \
-    openfaas \
+    faas-share \
     ./chart/faas-share \
-    --namespace openfaas  \
+    --namespace faas-share  \
     --set basic_auth=true \
     --set functionNamespace=faas-share-fn \
     --set operator.create=$CREATE_OPERATOR
@@ -52,9 +52,9 @@ helm upgrade \
 
 if [ "${OPERATOR}" == "1" ]; then
 
-    kubectl --context "kind-$DEVENV" patch -n openfaas deploy/gateway \
+    kubectl --context "kind-$DEVENV" patch -n faas-share deploy/gateway \
       -p='[{"op": "add", "path": "/spec/template/spec/containers/1/command", "value": ["./faas-share", "-operator=true"]} ]' --type=json
 fi
 
-kubectl --context "kind-$DEVENV" rollout status deploy/prometheus -n openfaas
-kubectl --context "kind-$DEVENV" rollout status deploy/gateway -n openfaas
+kubectl --context "kind-$DEVENV" rollout status deploy/prometheus -n faas-share
+kubectl --context "kind-$DEVENV" rollout status deploy/gateway -n faas-share
